24668afbf096cd31435200069ad01f844e523d48
Experio2705 <aryanshinde2705@gmail.com>
2026-02-06 16:48:27 +0530

    fix cors

--- server/db.js
+++ server/db.js
@@ -11,7 +11,8 @@ app.use(cors({
   methods: ["GET","POST","PUT","DELETE"],
   credentials: true
 }));
-app.options('*', cors());
+app.options(/.*/, cors());
+
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
