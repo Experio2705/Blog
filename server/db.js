db615c60d3abd97e8c8d721ca3f0a939e6c09fee
Experio2705 <aryanshinde2705@gmail.com>
2026-02-21 13:11:29 +0530

    Fixed upload image bug 3

--- server/db.js
+++ server/db.js
@@ -17,7 +17,7 @@ app.use(cors({
 }));
 app.options(/.*/, cors());
 
-cloudinary.v2.config({
+cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
