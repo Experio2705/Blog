4216953119b7380d05bfa7c104c901e3925808c6
Experio2705 <aryanshinde2705@gmail.com>
2026-02-21 13:02:24 +0530

    Fixed upload image bug 3

--- server/db.js
+++ server/db.js
@@ -3,6 +3,8 @@ import mysql from "mysql2"
 import cors from "cors";
 import multer from "multer";
 import dotenv from "dotenv";
+import cloudinary from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
 dotenv.config();
 
 
@@ -14,9 +16,7 @@ app.use(cors({
 }));
 app.options(/.*/, cors());
 
-const cloudinary=require("cloudinary").v2;
-const {CloudinaryStorage}=require("multer-storage-cloudinary");
-cloudinary.config({
+cloudinary.v2.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
@@ -44,7 +44,7 @@ db.connect(err=>{
 //   }
 // });
 const storage = new CloudinaryStorage({
-    cloudinary: cloudinary,
+    cloudinary: cloudinary.v2,
     params: {
         folder: "blog_images",
         allowed_formats: ["jpg", "png", "jpeg"],
